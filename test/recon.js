2c148b372093096db6814fd911fa19f571943c1d
James Halliday <substack@gmail.com>
2010-12-17 07:27:49 +0000

    failing test for buffered writes

---
+++ test/recon.js
@@ -0,0 +1,54 @@
+var recon = require('recon');
+var net = require('net');
+
+exports.buffered_write = function (assert) {
+    var port = Math.floor(10000 + Math.random() * (Math.pow(2,16) - 10000));
+    
+    var conn = recon(port);
+    var bufs = [];
+    
+    conn.on('data', function (buf) {
+        bufs.push(buf.toString());
+        setTimeout(function () {
+            conn.write(buf);
+            if (buf.toString() == 'end') {
+                conn.end();
+                assert.eql(bufs, [ 'pow!', 'end' ]);
+            }
+        }, 25);
+    });
+    
+    setTimeout(function () {
+        var server1 = net.createServer(function (stream) {
+            stream.write('pow!');
+            stream.destroy();
+            server1.close();
+            
+            stream.on('data', function () {
+                assert.fail('wrote before it was supposed to');
+            });
+        });
+        server1.listen(port);
+        
+        server1.on('close', function () {
+            setTimeout(function () {
+                var server2 = net.createServer(function (stream) {
+                    stream.setNoDelay();
+                    
+                    stream.write('end');
+                    var recv = [];
+                    stream.on('data', function (buf) {
+                        recv.push(buf.toString());
+                    });
+                    
+                    setTimeout(function () {
+                        stream.destroy();
+                        server2.close();
+                        assert.eql(recv, [ 'pow!', 'end' ]);
+                    }, 50);
+                });
+                server2.listen(port);
+            }, 100);
+        });
+    });
+};
