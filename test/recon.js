85daea877096b8b35b9c19fee4a8f9789d12ceeb
James Halliday <mail@substack.net>
2012-05-04 09:44:45 -0700

    now using tap and the test passes

--- test/recon.js
+++ test/recon.js
@@ -1,12 +1,13 @@
 var recon = require('recon');
+var test = require('tap').test;
 var net = require('net');
 
-exports.buffered_write = function (assert) {
+test('buffered write', function (t) {
     var port = Math.floor(10000 + Math.random() * (Math.pow(2,16) - 10000));
     
     var cb_count = 0;
     var conn = recon(port, function (c) {
-        assert.ok(c instanceof net.Stream);
+        t.ok(c instanceof net.Stream);
         cb_count ++;
     });
     var bufs = [];
@@ -17,38 +18,25 @@ exports.buffered_write = function (assert) {
             conn.write(buf);
             if (buf.toString() == 'end') {
                 conn.end();
-                assert.eql(bufs, [ 'pow!', 'end' ]);
+                t.same(bufs, [ 'pow!', 'end' ]);
             }
         }, 25);
     });
     
     var events = [];
     
-    var cto = setTimeout(function () {
-        assert.fail('never connected');
-    }, 5000);
-    
     conn.on('connect', function () {
-        clearTimeout(cto);
         events.push('connect');
     });
     
-    var dto = setTimeout(function () {
-        assert.fail('never dropped');
-    }, 5000);
     conn.on('drop', function () {
-        clearTimeout(dto);
         events.push('drop');
     });
     
-    var rcto = setTimeout(function () {
-        assert.fail('never reconnected');
-    }, 5000);
     conn.on('reconnect', function () {
-        clearTimeout(rcto);
         events.push('reconnect');
         
-        assert.eql(events, [ 'connect', 'drop', 'reconnect' ]);
+        t.same(events, [ 'connect', 'drop', 'reconnect' ]);
     });
     
     setTimeout(function () {
@@ -56,9 +44,10 @@ exports.buffered_write = function (assert) {
             stream.write('pow!');
             stream.destroy();
             server1.close();
+            t.end();
             
             stream.on('data', function () {
-                assert.fail('wrote before it was supposed to');
+                t.fail('wrote before it was supposed to');
             });
         });
         server1.listen(port);
@@ -76,12 +65,12 @@ exports.buffered_write = function (assert) {
                         conn.destroy();
                         stream.destroy();
                         server2.close();
-                        assert.eql(recv, [ 'pow!', 'end' ]);
-                        assert.eql(cb_count, 3);
+                        t.same(recv, [ 'pow!', 'end' ]);
+                        t.equal(cb_count, 3);
                     }, 50);
                 });
                 server2.listen(port);
             }, 100);
         });
     }, 100);
-};
+});
